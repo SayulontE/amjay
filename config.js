cace7c40208e8251d86b823358d147f751bc0c99
Sayu LontE <rasyadgajahmanik@gmail.com>
2022-05-07 16:54:31 +0700

    amjay

---
+++ config.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285161426785']
+global.premium = ['0']
+global.ownername = 'RasyadWiBu'
+global.botname = 'Elaina'
+global.packname = '© Elaina - Bot'
+global.gc = 'https://bit.ly/3KMlmv2'
+global.limitawal = '100'
+global.author = '@elaina.jpeg\nMau Nyolong kah 🧐📸'
+global.sessionName = 'sessionya'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⌕'
+global.mess = {
+    success: 'Berhasil',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu sebentar, sedang di proses',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.sc = fs.readFileSync('./media/sc.jpg')
+global.tq = fs.readFileSync('./media/tq.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.thumb = fs.readFileSync('./media/pepe.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
